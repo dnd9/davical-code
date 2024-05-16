5e74c9abdece1d57a67cd7f8cd0285322b9eac92
dnd9 <dnamanya.daniel@gmail.com>
2024-05-16 07:53:25 +0300

    davical.py

--- davical.py
+++ davical.py
@@ -9,37 +9,6 @@ import xml.etree.ElementTree as ET
 from vobject import readOne
 
 
-'''
-sample card data 
-'''
-vcard = '''
-    BEGIN:VCARD
-    VERSION:4.0
-    PRODID:-//Thunderbird.net/NONSGML Thunderbird CardBook V86.0
-    FN:Davical Main
-    N:Davical;Main;;;
-    EMAIL;PREF=1:goodman@noman.ca
-    REV:20230617T132145Z
-    END:VCARD
-    '''
-
-'''
-sample Calendar event data , ical data
-Important to know and consider that calendar server time zone is UTC
-'''
-event_data = """BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//davical5 Corp.//CalDAV Client//EN
-BEGIN:VEVENT
-SUMMARY:new event 2
-DTSTART:20230710T200000Z
-DTEND:20230710T210000Z
-LOCATION:Example Location
-DESCRIPTION:This is an update of the existing card.
-END:VEVENT
-END:VCALENDAR"""
-
-
 # Set the XML body for the PROPFIND request
 xml_body = '''<?xml version="1.0" encoding="UTF-8"?>
 <D:propfind xmlns:D="DAV:">
@@ -50,19 +19,17 @@ xml_body = '''<?xml version="1.0" encoding="UTF-8"?>
 </D:propfind>'''
 
 
+
+
 class DavicalClient:
-    def __init__(self, httplink, username, password ):
+    def __init__(self, httplink, username, password):
         self.httplink = httplink 
         self.username = username
         self.password = password
-        self.card_endpoint = '/test@scom.ca/addresses/' 
-        self.calendar_endpoint = '/test@scom.ca/calendar/'
         self.vcard_id_chars = string.ascii_letters + string.digits
-        self.calendar_url = self.httplink + self.calendar_endpoint 
-        self.card_url = self.httplink + self.card_endpoint
+     
     
-    
-    def make_request(self, method, endpoint, data):
+    def make_request(self, method, endpoint, data = None):
         url = self.httplink 
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -74,17 +41,23 @@ class DavicalClient:
         response.raise_for_status()
         return response
    
-    def addcard(self, vcard): 
+    def addcard(self, card_name, vcard): 
+         
         '''
         * innitiate an object
         * serelialize  Vcard as a string 
         * send a put request to save the Vcard to the davical server
         '''
+
+        endpoint = '/{username}/{card_name}/'
+        endpoint = endpoint.format(username=self.username,card_name = card_name)
         vcard_id = self.generate_vcard_id()
         v = vobject.readOne(vcard)
         vcard_str = v.serialize()
-        url = self.httplink + self.card_endpoint + vcard_id + ".vcf"
+        url = self.httplink + endpoint + vcard_id + ".vcf"
+        
         result = requests.put(url, auth=(self.username, self.password), data = vcard_str, headers={'Content-Type': 'text/vcard'})
+       
         if result.status_code in [200, 201]:   
             return result.status_code
         else:
@@ -96,7 +69,7 @@ class DavicalClient:
         vcard_id = '-'.join([vcard_id[i:i+4] for i in range(0, len(vcard_id), 4)])
         
         try:
-            carddav = self.make_request("PROPFIND", self.httplink + vcard_id + '.vcf', vcard)
+            carddav = self.make_request("PROPFIND", self.httplink + vcard_id + '.vcf' )
 
             if carddav is None:
                 vcard_id = self.generate_vcard_id()
@@ -105,7 +78,8 @@ class DavicalClient:
             print(f"Connection error occurred: {str(e)}")   
 
 
-    def addcalendar(self, event_data):
+    def addcalendar(self, calendar_name, event_data):
+        
         '''
          Extract the start date from the event data
          Convert event start date to datetime object
@@ -116,6 +90,10 @@ class DavicalClient:
          Split the summary into words
          generate filename 
         '''
+
+        endpoint = '/{username}/{calendar_name}/'
+        endpoint = endpoint.format(username=self.username,calendar_name = calendar_name)
+        calendar_url = self.httplink + endpoint 
         start_date_index = event_data.find("DTSTART:") + len("DTSTART:")
         end_date_index = event_data.find("Z", start_date_index)
         event_start_date = event_data[start_date_index:end_date_index]
@@ -127,10 +105,9 @@ class DavicalClient:
         separator = "_"
         summary_words = summary.split()  
         joined_summary = separator.join(summary_words)
-        event_url = self.calendar_url + joined_summary + '.ics'
-       
-        current_datetime = datetime.utcnow()
+        event_url = calendar_url + joined_summary + '.ics'
         
+        current_datetime = datetime.utcnow()
         if event_start_datetime >= current_datetime:  
             result = requests.put(event_url, auth=(self.username, self.password), data = event_data, headers={'Content-Type': 'text/calendar'})
             if result.status_code in [200, 201, 203, 204, 205, 206, 207, 208, 226] :   
@@ -141,6 +118,22 @@ class DavicalClient:
         else:
             return "Event schedule time should not be in the past "
 
+    def retrieve_single_calendar_event(self,summary):
+        separator = "_"
+        summary_words = summary.split()  # Split the summary into words
+        joined_summary = separator.join(summary_words)
+        endpoint = f"/{self.username}/calendar/{joined_summary}"
+        event_url = self.httplink + endpoint + '.ics'
+
+        if event_url is not None:              
+            response = requests.get(event_url, auth=(self.username,self.password))
+            if response.status_code == 200:
+                response = response.text
+            else:
+                print(f"Error: {response.status_code}")
+        else:
+             print('Card does not exist')
+        return response
 
     def searchcalendar(self, calendar_name):
         '''
@@ -152,8 +145,9 @@ class DavicalClient:
         Process the iCal data as needed
 
         '''
-        calendar_name = 'Scom test Account calendar'    
-        client = DAVClient(url=self.httplink, username=self.username, password=self.password)
+        calendar_name = 'Scom Test Account calendar' 
+        url = self.httplink 
+        client = DAVClient(url, username = self.username, password = self.password)
         principal = client.principal()
         calendars = principal.calendars()
         calendar = None
@@ -161,19 +155,20 @@ class DavicalClient:
             if cal.name == calendar_name:
                 calendar = cal
                 break
-
         if calendar is None:
             print("Calendar not found.")
             exit()
-        events = calendar.search()
+        events = calendar.events()
+        return events
         calendar_events = []
         for event in events:
-            event_data = event.data
+            event_data = event.text
             calendar_events.append(event_data)
-        return calendar_events
+        return event_data
 
 
-    def searchcard(self):
+
+    def searchcard(self, card_name):
         '''
         Set up connection parameters
         Send the PROPFIND request 
@@ -182,8 +177,9 @@ class DavicalClient:
         Parse the VCF file using the vobject library
         Extract fields from the vCard
         '''
-        
-        url = self.httplink  + self.card_endpoint
+        endpoint = '/{username}/{card_name}/'
+        endpoint = endpoint.format(username=self.username,card_name = card_name)
+        url = self.httplink  + endpoint
         headers = {
             'Content-Type': 'application/xml',
             'Depth': '1'
@@ -217,44 +213,129 @@ class DavicalClient:
 
 
 
-    def delcal(self, summary):
+    def delcal(self, calendar_name, summary):
         separator = "_"
         summary_words = summary.split()  
         joined_summary = separator.join(summary_words)
-        username = 'test@scom.ca'
-        password = 'Testing123'
-        endpoint = f"/{username}/calendar/{joined_summary}"
-        event_url = self.httplink  +endpoint + '.ics'
-
+        endpoint = '/{username}/{calendar_name}/'
+        endpoint = endpoint.format(username=self.username,calendar_name = calendar_name) + joined_summary
+        event_url = self.httplink  + endpoint + '.ics'
         if event_url is not None:              
-            response = requests.delete(event_url, auth=(username,password))
-            if response.status_code == 201:
+            response = requests.delete(event_url, auth=(self.username,self.password))
+            if response.status_code in (201, 204):
                 print(f"Event'{summary}' has been deleted calendar.")
             else:
         
                 print(f"Failed to delete event '{summary}' from calendar.")
                 print(f"Error: {response.status_code}")
         else:
-            print('Card does not exist')
+            return 'Card does not exist'
         
     
-    def delcard(self, calendar_name, card_name):
-        endpoint = "/{username}/{calendar_name}/{card_name}.vcf"
-        endpoint = endpoint.format(username=self.username, calendar_name=calendar_name, card_name = card_name)
-        self.make_request("DELETE", endpoint)
-
-        
-davclient = DavicalClient('https://davical.scom.ca', 'test@scom.ca', 'Testing123' )
-
-try:
-    # events = davclient.searchcalendar('calendar')
-    # for event in events:
-    #     print(event)
-    # print(davclient.addcalendar(event_data))
-    print (davclient.delcal('new event 2'))
-    # print (davclient.addcard('addresses'))
-except requests.exceptions.RequestException as e:
-    print("Connection Error:", e)
+    def delcard(self, card_name, card_id):
+        #https://davical.scom.ca/test@scom.ca/addresses/
+        endpoint = "/{username}/{card_name}/{card_id}"
+        endpoint = endpoint.format(username = self.username, card_name = card_name, card_id = card_id)+ '.vcf'
+        address_book_url = self.httplink + endpoint
+        if card_id is not None:
+            try:         
+                response = requests.delete(address_book_url, auth=(self.username,self.password))
+                print(f"vCard '{card_id}' has been deleted from addresses books.")
+            except requests.HTTPError as e:
+                print(f"Failed to delete vCard '{card_id}' from calendar addresses books.")
+                print(f"Error: {e}")
+        else:
+             print('Card does not exist')
+        return response
 
 
+    def update_calendar_event(self,summary, new_event_data):
+        separator = "_"
+        url = 'https://davical.scom.ca/'
+        summary_words = summary.split()  # Split the summary into words
+        joined_summary = separator.join(summary_words)
+        endpoint = f"/{self.username}/calendar/{joined_summary}"
+        event_url = self.httplink + endpoint + '.ics'
 
+        if event_url is not None:
+            response = requests.get(event_url, auth=(self.username, self.password))
+            if response.status_code == 200:
+                response_text = response.text
+                updated_event = self.modify_event(response_text, new_event_data)
+                update_response = requests.put(event_url, auth=(self.username, self.password), data=updated_event)
+                
+                if update_response.status_code == 204:
+                    print(f"Event '{summary}' updated successfully.")
+                else:
+                    print(f"Failed to update event '{summary}' from calendar.")
+                    print(f"Error: {update_response.status_code}")
+            else:
+                print(f"Failed to retrieve event '{summary}' from calendar.")
+                print(f"Error: {response.status_code}")
+        else:
+            print('Card does not exist')
+
+    def modify_event(self,event_data, new_event_data):
+        modified_event = event_data
+
+        for attribute, value in new_event_data.items():
+            attribute_string = f"{attribute.upper()}:"
+            if attribute_string in modified_event:
+                start_index = modified_event.index(attribute_string) + len(attribute_string)
+                end_index = modified_event.index("\n", start_index)
+                existing_value = modified_event[start_index:end_index]
+                modified_event = modified_event.replace(existing_value, value)
+
+        return modified_event
+
+
+    def retrieve_single_card(self, vcard_id, card_name):      
+        endpoint = f"/{self.username}/{card_name}/"
+        card_url = self.httplink + endpoint +vcard_id + ".vcf"
+        if card_url is not None:              
+            response = requests.get(card_url, auth=(self.username,self.password))
+            if response.status_code == 200:
+                response = response.text
+            else:
+                print(f"Error: {response.status_code}")
+        else:
+             print('Card does not exist')
+        return response
+
+
+    def update_card(self, vcard_id, new_card_data): 
+        endpoint = f"/{self.username}/addresses/"
+        card_url = self.httplink+ endpoint + vcard_id + ".vcf"
+
+        if card_url is not None:
+            response = requests.get(card_url, auth=(self.username, self.password))
+            if response.status_code == 200:
+                response_text = response.text
+                updated_card = self.modify_card(response_text, new_card_data)
+                update_response = requests.put(card_url, auth=(self.username, self.password), data=updated_card)
+                
+                if update_response.status_code == 204:
+                    response = f"Event '{vcard_id}' updated successfully."
+                else:
+                    response = f"Error: {update_response.status_code}"
+            else:
+                response= f"Error: {response.status_code}"
+        else:
+            print('Card does not exist')
+        return response
+
+
+    def modify_card(self,card_data, new_card_data):
+        modified_card = card_data
+
+        for attribute, value in new_card_data.items():
+            attribute_string = f"{attribute.upper()}:"
+            if attribute_string in modified_card:
+                start_index = modified_card.index(attribute_string) + len(attribute_string)
+                end_index = modified_card.index("\n", start_index)
+                existing_value = modified_card[start_index:end_index]
+                modified_card = modified_card.replace(existing_value, value)
+
+        return modified_card
+
+            
